fdd0fac9bad9791ec4f90361eeaf24db86869c6d
joao <joao182cx@gmail.com>
2019-01-23 11:44:24 -0200

    Criação do layout do aplicativo.

--- src/EcofontFrame.java
+++ src/EcofontFrame.java
@@ -1,14 +1,45 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GridLayout;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class EcofontFrame extends JFrame{
+	
+	JPanel panelBotoes;
 	
 	JButton botaoAumentar;
 	JButton botaoDiminuir;
 	JTextArea campoEcofont;
 	Font ecofont;
+	
+	public EcofontFrame() {
+		
+		super("Ecofont");
+		setLayout(new BorderLayout());
+		
+		panelBotoes = new JPanel(new GridLayout(1, 2));
+		add(panelBotoes, BorderLayout.NORTH);
+		panelBotoes.setLayout(new FlowLayout());
+		
+		botaoAumentar = new JButton("Aumentar fonte");
+		botaoDiminuir = new JButton("Diminuir fonte");
+		panelBotoes.add(botaoAumentar);
+		panelBotoes.add(botaoDiminuir);
+		
+		ecofont = new Font("Serif", Font.BOLD, 15);
+		
+		campoEcofont = new JTextArea();
+		campoEcofont.setFont(ecofont);
+		
+		add(campoEcofont);
+		add(campoEcofont, BorderLayout.CENTER);
+	}
 	
 }
---
+++ src/EcofontTeste.java
@@ -0,0 +1,16 @@
+import java.awt.FontFormatException;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+public class EcofontTeste {
+
+	public static void main(String[] args) {
+		
+		EcofontFrame eco = new EcofontFrame();
+		eco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		eco.setSize(400, 400);
+		eco.setVisible(true);
+	}
+
+}
