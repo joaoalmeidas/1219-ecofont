9b28a14037b3884ae12c79a45401abf9be8350c5
joao <joao182cx@gmail.com>
2019-01-23 13:09:57 -0200

    Adição dos ActionListener aos botões.

--- src/EcofontFrame.java
+++ src/EcofontFrame.java
@@ -1,9 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.FontFormatException;
 import java.awt.GridLayout;
-import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,7 +17,6 @@ public class EcofontFrame extends JFrame{
 	JButton botaoAumentar;
 	JButton botaoDiminuir;
 	JTextArea campoEcofont;
-	Font ecofont;
 	
 	public EcofontFrame() {
 		
@@ -33,13 +32,47 @@ public class EcofontFrame extends JFrame{
 		panelBotoes.add(botaoAumentar);
 		panelBotoes.add(botaoDiminuir);
 		
-		ecofont = new Font("Serif", Font.BOLD, 15);
 		
 		campoEcofont = new JTextArea();
-		campoEcofont.setFont(ecofont);
+		campoEcofont.setFont(new Font("Serif", Font.BOLD, 10));
 		
 		add(campoEcofont);
 		add(campoEcofont, BorderLayout.CENTER);
+		
+		botaoAumentar.addActionListener(
+				
+				new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						
+						campoEcofont.setFont(new Font("Serif", Font.BOLD, campoEcofont.getFont().getSize() + 1));
+						
+						repaint();
+					}
+					
+					
+				}
+				
+		);
+		
+		botaoDiminuir.addActionListener(
+				
+				new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						
+						campoEcofont.setFont(new Font("Serif", Font.BOLD, campoEcofont.getFont().getSize() - 1));
+						
+						repaint();
+					}
+					
+					
+				}
+				
+		);
 	}
+	
 	
 }
