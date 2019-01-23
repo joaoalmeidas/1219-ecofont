e7662c1a2141cc47e47b8c589420b6591ce7b46e
joao <joao182cx@gmail.com>
2019-01-23 11:13:43 -0200

    Criação da classe EcofontFrame com as suas respectivas variáveis.

---
+++ src/EcofontFrame.java
@@ -0,0 +1,14 @@
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class EcofontFrame extends JFrame{
+	
+	JButton botaoAumentar;
+	JButton botaoDiminuir;
+	JTextArea campoEcofont;
+	Font ecofont;
+	
+}
